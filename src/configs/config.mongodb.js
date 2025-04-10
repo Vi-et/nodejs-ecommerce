b6a19683605a67a36216723afdf3a6496d08ff08
Viet Dang Nhat <nhatviet.mkt@gmail.com>
2025-04-10 22:15:53 +0700

    feat: setup env va config

---
+++ src/configs/config.mongodb.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const { model } = require("mongoose");
+
+const dev = {
+    app : {
+        port : process.env.DEV_APP_PORT || 3000,
+    },
+    db : {
+        host : process.env.DEV_DB_HOST || 'localhost',
+        port : process.env.DEV_DB_PORT || 27017,
+        name : process.env.DEV_DB_NAME || 'dbDev'
+    }
+}
+
+const prod = {
+    app : {
+        port : process.env.PROD_APP_PORT || 3000,
+    },
+    db : {
+        host : process.env.PROD_DB_HOST || 'localhost',
+        port : process.env.PROD_DB_PORT || 27017,
+        name : process.env.PROD_DB_NAME || 'dbProd'
+    }
+}
+
+const config = {
+    dev,
+    prod
+}
+
+const env = process.env.NODE_ENV || 'dev';
+module.exports = config[env];
--- src/dbs/init.mongo.js
+++ src/dbs/init.mongo.js
@@ -1,9 +1,10 @@
 'use strict'
 
 const mongoose = require('mongoose');
+const {db : {host, port, name}} = require('../configs/config.mongodb');
 const {countConnect} = require('../helpers/check.connect');
 
-const connectString = 'mongodb://localhost:27017/shopDEV';
+const connectString = `mongodb://${host}:${port}/${name}`;
 
 
 class Database{
@@ -16,6 +17,7 @@ class Database{
         mongoose.set('debug', {color: true});
 
         mongoose.connect(connectString).then(_ => {
+            console.log(connectString)
             console.log('Connected to MongoDB', countConnect());
         }).catch(err => {
             console.error(err);
