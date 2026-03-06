a048384c0dd5c3f919fc7439b1ae119f4914b7f7
vi-et <nv.mkt@gmail.com>
2026-03-06 09:56:26 +0700

    feat: cloudfront signed url

--- src/services/s3.service.js
+++ src/services/s3.service.js
@@ -1,5 +1,7 @@
-const {s3Client, PutObjectCommand, GetObjectCommand} = require('../configs/s3.config')
-const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const {s3Client, PutObjectCommand} = require('../configs/s3.config')
+const {getSignedUrl} = require('@aws-sdk/cloudfront-signer')
+const cloudFrontUrl = 'd1ayvk1bqe4nyr.cloudfront.net'
+const dateLessThan = new Date(Date.now() + 60 * 60 * 24 * 7 * 1000) // 1 days
 
 const uploadImageS3 = async ({file}) => {
     try{
@@ -13,12 +15,14 @@ const uploadImageS3 = async ({file}) => {
     // You deleted the logic to send the upload request here! You need it first before you can fetch the image.
     await s3Client.send(command)
 
-    const urlGet = new GetObjectCommand({
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: file.originalname,
-    })
-    const url = await getSignedUrl(s3Client, urlGet, { expiresIn: 60 * 60 * 24 * 7 })
-    return url
+    const signedUrl = getSignedUrl({
+  url: `https://${cloudFrontUrl}/${file.originalname}`,
+  keyPairId: process.env.AWS_CLOUDFRONT_KEY_PAIR_ID,
+  dateLessThan,
+  privateKey : process.env.AWS_CLOUDFRONT_PRIVATE_KEY,
+});
+
+    return signedUrl
     }catch(error){
         console.log(error)
     }
